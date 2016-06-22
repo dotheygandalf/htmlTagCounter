472eb7da32432e6d0eb7d167e1bf6b7546e3dd81
Jonathan Chang <jchang@parasoft.com>
2016-06-22 11:35:03 -0700

    better error handling and initial state

--- bin/app-templates.js
+++ bin/app-templates.js
@@ -12,12 +12,12 @@ angular.module("directives/tagCountViewer/tagCountViewer.tpl.html", []).run(["$t
     "<div class=\"tag-count-viewer\">\n" +
     "  <div class=\"row\">\n" +
     "    <div class=\"col-lg-12\">\n" +
-    "      <form novalidate name=\"urlForm\">\n" +
+    "      <form novalidate name=\"urlForm\" ng-submit=\"go()\">\n" +
     "        <label>Get Source from URL</label>\n" +
     "        <div class=\"input-group\">\n" +
-    "          <input class=\"form-control\" name=\"url\" ng-model=\"url\" type=\"url\">\n" +
+    "          <input class=\"form-control\" name=\"url\" ng-model=\"url\" placeholder=\"https://www.website.abc\" type=\"url\" required=\"required\">\n" +
     "          <span class=\"input-group-btn\">\n" +
-    "            <button class=\"btn btn-default\" type=\"button\" ng-click=\"getTagCount()\">Go!</button>\n" +
+    "            <button class=\"btn btn-default\" type=\"button\" ng-click=\"go()\">Go!</button>\n" +
     "          </span>\n" +
     "        </div>\n" +
     "      </form>\n" +
@@ -25,9 +25,20 @@ angular.module("directives/tagCountViewer/tagCountViewer.tpl.html", []).run(["$t
     "  </div>\n" +
     "\n" +
     "  <div class=\"row\">\n" +
-    "    <div class=\"col-lg-12\">\n" +
+    "    <div class=\"col-lg-12\" ng-if=\"!error && !isLoading\">\n" +
     "      <code-viewer content=\"html\" class=\"col-lg-10\"></code-viewer>\n" +
     "      <tag-list tags=\"tags\" ng-if=\"tags\" class=\"col-lg-2\"></tag-list>\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"col-lg-12\" ng-if=\"error && !isLoading\">\n" +
+    "      <h1>Woops</h1>\n" +
+    "      <h4>It looks like there was a problem retrieving your reqested page.</h4>\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"col-lg-12\" ng-if=\"isLoading\">\n" +
+    "      <div class=\"spinner\">\n" +
+    "        <i class=\"fa fa-circle-o-notch fa-spin\" aria-hidden=\"true\"></i>\n" +
+    "      </div>\n" +
     "    </div>\n" +
     "  </div>\n" +
     "</div>");
--- client/src/directives/tagCountViewer/tagCountViewer.directive.js
+++ client/src/directives/tagCountViewer/tagCountViewer.directive.js
@@ -1,29 +1,53 @@
 (function() {
   'use strict'
 
-  function tagCountViewerController($scope, $http) {
-    $scope.url = 'https://material.angularjs.org/latest/';
+  function tagCountViewerController($scope, $http, $q, $timeout) {
+    $scope.go = function(url) {
+      getTags(url || $scope.url);
+    };
 
-    $scope.$watch('url', function(url) {
-      if($scope.urlForm.$valid) {
-        getTags(url);
+    $scope.demo = function(url) {
+      getTags(url, true);
+    };
+
+    function getTags(url, demoOverride) {
+      if(demoOverride || $scope.urlForm.$valid) {
+        $scope.isLoading = true;
+        var deferred = $q.defer();
+
+        $http.get('/api/v1/tags', {
+          params: {
+            url: url
+          }
+        }).then(function(response) {
+          $scope.tags = response.data.elements;
+          $scope.html = response.data.html;
+          delete $scope.error;
+          $scope.urlForm.$setPristine();
+          deferred.resolve(response.data);
+        }, function(error) {
+          delete $scope.tags;
+          delete $scope.html;
+          $scope.error = 'BAD_REQUEST';
+          $scope.urlForm.url.$setValidity('badRequest', false);
+          deferred.reject();
+        })['finally'](function() {
+          $timeout(function() {
+            $scope.isLoading = false;
+          }, 300);
+        });
+
+        return deferred.promise;
       }
-    });
-
-    function getTags(url) {
-      $http.get('/api/v1/tags', {
-        params: {
-          url: url
-        }
-      }).then(function(response) {
-        $scope.tags = response.data.elements;
-        $scope.html = response.data.html;
-      });
+      return $q.reject();
     }
 
     $scope.$on('tagCountViewer:selectTag', function(event, tag) {
       $scope.$broadcast('selectTag', tag);
     });
+
+    $scope.url = 'https://material.angularjs.org/latest/';
+    $scope.demo($scope.url);
   }
 
   angular.module('htmlTagCounterApp')
